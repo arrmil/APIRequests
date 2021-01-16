8afcd4362779a0216735a0467d330a712775993d
Arturas Milius <arturas114@gmail.com>
2021-01-16 22:30:49 +0200

    Initial Commit

---
+++ APIRequests/APIRequestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  APIRequestsApp.swift
+//  APIRequests
+//
+//  Created by Arturas Milius on 2021-01-16.
+//
+
+import SwiftUI
+
+@main
+struct APIRequestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
