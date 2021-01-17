23f898d89b89d00ceac6591d0969d0f895b2bb68
Arturas Milius <arturas114@gmail.com>
2021-01-17 21:38:44 +0200

    Api requests

--- APIRequests/ContentView.swift
+++ APIRequests/ContentView.swift
@@ -8,44 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var channels: [Programme] = []
+    
     var body: some View {
-            Checkerboard(rows: 5, columns: 5)
-                .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
-//                .fill(Color.red)
-                .frame(width: 200, height: 200)
-        }
-        
-//        Text("Hello, world!")
-//            .onAppear() {
-//                API().getUsers()
-//            }
-}
-
-struct Checkerboard: Shape {
-    let rows: Int
-    let columns: Int
-
-    func path(in rect: CGRect) -> Path {
-        var path = Path()
-
-        // figure out how big each row/column needs to be
-        let rowSize = rect.height / CGFloat(rows)
-        let columnSize = rect.width / CGFloat(columns)
-        for column in 0 ..< columns {
-            for row in 0 ..< rows {
-                if(column == 0 || row == 0 || column == columns - 1 || row == rows - 1) {
-                    let startX = columnSize * CGFloat(column)
-                    let startY = rowSize * CGFloat(row)
-                    
-                    let rect = CGRect(x: startX, y: startY, width: columnSize, height: rowSize)
-                    path.addRect(rect)
-                }
+        List(channels, id: \.self) { channel in
+            if(channel.title_original.count > 0) {
+                Text(channel.title_original)
             }
         }
+        .onAppear {
+            API().getChannels{ channels in
+                self.channels = channels
 
-        return path
+            }
+        }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
--- APIRequests/Data.swift
+++ APIRequests/Data.swift
@@ -7,20 +7,35 @@
 
 import SwiftUI
 
-struct User: Codable,Identifiable {
+struct Channel: Codable, Identifiable {
     let id = UUID()
-    var name: String
-    var email: String
+    var nextDate: String
+    var previousDate: String
+
+    var schedule: Schedule
+}
+
+struct Schedule: Codable {
+    var dayName: String
+    var date: String
+    var programme: [Programme]
+}
+
+struct Programme: Codable, Hashable {
+    var title: String
+    var title_original: String
 }
 
 
 class API {
-    func getUsers() {
-        guard let url = URL(string: "https://jsonplaceholder.typicode.com/users") else { return }
-        
+    func getChannels(completion: @escaping ([Programme]) -> ()) {
+        guard let url = URL(string: "https://www.tv24.lt/programme/listing/none/17-01-2021?filter=channel&subslug=tv3-2") else { return }
+
         URLSession.shared.dataTask(with: url) { (data, _, _) in
-            let users = try!JSONDecoder().decode([User].self, from: data!)
-            print(users)
+            let channel = try!JSONDecoder().decode(Channel.self, from: data!)
+            DispatchQueue.main.async {
+                completion(channel.schedule.programme)
+            }
         }
         .resume()
     }
